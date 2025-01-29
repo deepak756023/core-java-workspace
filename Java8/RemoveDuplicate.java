110e6d4f750e84c0749d0fa5ba1ff03c5bc02478
deepak-nayak <deepaknayak55260@gmail.com>
2025-01-29 07:42:05 +0530

    practice problems in Java8 features

--- Java8/RemoveDuplicate.java
+++ Java8/RemoveDuplicate.java
@@ -6,7 +6,9 @@ public class RemoveDuplicate {
     public static void main(String[] args) {
         List<String> listOfStrings = Arrays.asList("Java", "Python", "C#", "Java", "Kotlin", "Python");
 
-        List<String> uniqueStrngs = listOfStrings.stream().distinct().collect(Collectors.toList());
+        List<String> uniqueStrngs = listOfStrings.stream()
+                .distinct().
+                collect(Collectors.toList());
 
         System.out.println(uniqueStrngs);
     }
